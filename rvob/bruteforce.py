a6cb15dcbdda2d0ebb5f81054bdfe189d79e1200
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2022-04-29 10:31:12 +0200

    Cleanup Put everything in a class to avoid passing tons of parameters and for better readability

--- rvob/bruteforce.py
+++ rvob/bruteforce.py
@@ -5,6 +5,49 @@ from ast import literal_eval
 from deton import execute
 from multiprocessing import Pool
 
+
+class Configuration:
+    def __init__(self, input_file, register_scrumbling, constant_obfuscation, obfuscation_chain_length, garbage_blocks, garbage_length, id):
+        self.input_file = input_file
+        self.register_scrumbling = register_scrumbling
+        self.constant_obfuscation = constant_obfuscation
+        self.obfuscation_chain_length = obfuscation_chain_length
+        self.garbage_blocks = garbage_blocks
+        self.garbage_length = garbage_length
+        self.id = id
+
+    def get_parameters(self):
+        return (
+            self.input_file, 
+            self.register_scrumbling, 
+            self.constant_obfuscation, 
+            self.obfuscation_chain_length, 
+            self.garbage_blocks, 
+            self.garbage_length
+        )
+
+    def evaluate(self):
+        output_name = f"_{self.id % 100}" if isinstance(self.id, int) else f"_{self.id}"
+        execute(
+            self.input_file, "", 50, 
+            self.register_scrumbling, self.constant_obfuscation, self.garbage_blocks, self.garbage_length,
+            path.dirname(__file__) + '/metrics/output.s', False, True, output_name, self.obfuscation_chain_length
+        )
+        with open(path.dirname(__file__) + f'/metrics/data_metrics{output_name}.txt') as f:
+            data = f.read()
+            
+        mean_heats = literal_eval(re.search(r"Mean heat after: (\[.*\])", data).group(1))
+        self.mean_heat = sum(mean_heats) / 32
+
+        with open(path.dirname(__file__) + '/metrics/output.s') as f:
+            self.lines_num = f.read().count("\n")
+        
+        return self
+
+    def __str__(self):
+        return f"{self.register_scrumbling} {self.constant_obfuscation} {self.obfuscation_chain_length} {self.garbage_blocks} {self.garbage_length}"
+
+
 def get_args():
     parser = argparse.ArgumentParser(description="Bruteforce of DETON")
 
@@ -33,66 +76,45 @@ def get_args():
 
     return parser.parse_args()
 
-def run_evaluate(input_file, original_lines_num, register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length, iteration, total_iterations):
-    mean_heat, lines_num = evaluate(input_file, register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length)
-    print(input_file)
-    print(f"Executed with: {register_scrumbling} {constant_obfuscation} {garbage_blocks} {garbage_length} ({iteration} / {total_iterations})")
-    print("New mean heat:", mean_heat)
-    print("Overhead introduced:", lines_num - original_lines_num)
+def save_if_best(configuration):
+    global best, original_configuration, total_iterations
+    print(f"Executed with: {configuration} ({configuration.id} / {total_iterations}")
+    print("New mean heat:", configuration.mean_heat)
+    print("Overhead introduced:", configuration.lines_num - original_configuration.lines_num)
     print()
-    return mean_heat, lines_num, (register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length)
-
-best = None
-def save_if_best(values):
-    global best
-    mean_heat, lines_num, (register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length ,chain_length) = values
-    if best is None or mean_heat > best[0]:
-        best = (mean_heat, lines_num - original_lines_num, (register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length))
+    if configuration.mean_heat > best.mean_heat:
+        best = configuration
     
 
-def evaluate(input_file, register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length):
-    execute(
-        input_file, "", 50, 
-        register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length,
-        path.dirname(__file__) + '/metrics/output.s', False, True, "tmp", chain_length
-    )
-    with open(path.dirname(__file__) + '/metrics/data_metricstmp.txt') as f:
-        data = f.read()
-    mean_heats = literal_eval(re.search(r"Mean heat after: (\[.*\])", data).group(1))
-    mean_heat = sum(mean_heats) / 32
-
-    with open(path.dirname(__file__) + '/metrics/output.s') as f:
-        lines_num = f.read().count("\n")
-    return mean_heat, lines_num
-
-
 def main():
-    global original_lines_num
+    global best, original_configuration, total_iterations
     args = get_args()
     input_file = args.File
     threads = args.Threads
 
-    original_value, original_lines_num = evaluate(input_file, 0, 0, 0, 0, 0)
-    max_overhead = args.Overhead * original_lines_num // 100
+    original_configuration = Configuration(input_file, 0, 0, 0, 0, 0, "original")
+    original_configuration.evaluate()
+    best = original_configuration
+    max_overhead = args.Overhead * original_configuration.lines_num // 100
     print("Max absolute overhead:", max_overhead)
-    print("Original mean heat:", original_value)
+    print("Original mean heat:", original_configuration.mean_heat)
     print()
 
-    # Calculate totoa iterations
+    # Calculate total iterations
     total_iterations = 0
     for constant_obfuscation in range(0, max_overhead):
         if constant_obfuscation > max_overhead:
             break
         if constant_obfuscation > 0:
-            for chain_length in range(1, 20):
-                if constant_obfuscation * chain_length > max_overhead:
+            for obfuscation_chain_length in range(2, 20):
+                if constant_obfuscation * obfuscation_chain_length > max_overhead:
                     break
                 for garbage_blocks in range(0, max_overhead):
                     if garbage_blocks > 0:
-                        if constant_obfuscation * chain_length + garbage_blocks > max_overhead:
+                        if constant_obfuscation * obfuscation_chain_length + garbage_blocks > max_overhead:
                             break
                         for garbage_length in range(1, max_overhead):
-                            if constant_obfuscation * chain_length + garbage_blocks * garbage_length > max_overhead:
+                            if constant_obfuscation * obfuscation_chain_length + garbage_blocks * garbage_length > max_overhead:
                                 break
                             total_iterations += 1
                     else:
@@ -109,7 +131,6 @@ def main():
                 else:
                     total_iterations += 1
 
-
     # Try every combination
     iteration = 0
     thread_pool = Pool(threads)
@@ -118,33 +139,51 @@ def main():
             break
 
         if constant_obfuscation > 0:
-            for chain_length in range(1, 20):
-                if constant_obfuscation * chain_length > max_overhead:
+            for obfuscation_chain_length in range(2, 20):
+                if constant_obfuscation * obfuscation_chain_length > max_overhead:
                     break
 
                 for garbage_blocks in range(0, max_overhead):
                     if garbage_blocks > 0:
-                        if constant_obfuscation * chain_length + garbage_blocks > max_overhead:
+                        if constant_obfuscation * obfuscation_chain_length + garbage_blocks > max_overhead:
                             break
 
                         for garbage_length in range(1, max_overhead):
-                            if constant_obfuscation * chain_length + garbage_blocks * garbage_length > max_overhead:
+                            if constant_obfuscation * obfuscation_chain_length + garbage_blocks * garbage_length > max_overhead:
                                 break
 
-                            register_scrumbling = max_overhead - constant_obfuscation * chain_length - garbage_blocks * garbage_length
+                            register_scrumbling = max_overhead - constant_obfuscation * obfuscation_chain_length - garbage_blocks * garbage_length
                             iteration += 1
+                            configuration = Configuration(
+                                input_file, 
+                                register_scrumbling, 
+                                constant_obfuscation, 
+                                obfuscation_chain_length, 
+                                garbage_blocks, 
+                                garbage_length,
+                                iteration
+                            )
                             thread_pool.apply_async(
-                                run_evaluate,
-                                args = (input_file, original_lines_num, register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, chain_length, iteration, total_iterations),
+                                configuration.evaluate,
+                                args = (),
                                 callback = save_if_best
                             )
 
                     else:
-                        register_scrumbling = max_overhead - constant_obfuscation * chain_length
+                        register_scrumbling = max_overhead - constant_obfuscation * obfuscation_chain_length
                         iteration += 1
+                        configuration = Configuration(
+                            input_file, 
+                            register_scrumbling, 
+                            constant_obfuscation, 
+                            obfuscation_chain_length, 
+                            0, 
+                            0,
+                            iteration
+                        )
                         thread_pool.apply_async(
-                            run_evaluate,
-                            args = (input_file, original_lines_num, register_scrumbling, constant_obfuscation, 0, 0, chain_length, iteration, total_iterations),
+                            configuration.evaluate,
+                            args = (),
                             callback = save_if_best
                         )
         
@@ -160,18 +199,36 @@ def main():
 
                         register_scrumbling = max_overhead - garbage_blocks * garbage_length
                         iteration += 1
+                        configuration = Configuration(
+                            input_file, 
+                            register_scrumbling, 
+                            0, 
+                            0, 
+                            garbage_blocks, 
+                            garbage_length,
+                            iteration
+                        )
                         thread_pool.apply_async(
-                            run_evaluate,
-                            args = (input_file, original_lines_num, register_scrumbling, constant_obfuscation, garbage_blocks, garbage_length, 0, iteration, total_iterations),
+                            configuration.evaluate,
+                            args = (),
                             callback = save_if_best
                         )
 
                 else:
                     register_scrumbling = max_overhead
                     iteration += 1
+                    configuration = Configuration(
+                        input_file, 
+                        register_scrumbling, 
+                        0, 
+                        0, 
+                        0, 
+                        0,
+                        iteration
+                    )
                     thread_pool.apply_async(
-                        run_evaluate,
-                        args = (input_file, original_lines_num, register_scrumbling, constant_obfuscation, 0, 0, 0, iteration, total_iterations),
+                        configuration.evaluate,
+                        args = (),
                         callback = save_if_best
                     )
 
@@ -181,24 +238,22 @@ def main():
 
     print()
     print("DONE")
+    print("-" * 30)
     print("Best parameters:")
-    print("Register scrambling:", best[2][0])
-    print("Constant obfuscation:", best[2][1])
-    print("Garbage blocks:", best[2][2])
-    print("Garbage length:", best[2][3])
-    print("Chain length:", best[2][4])
+    print("Register scrambling:", best.register_scrumbling)
+    print("Constant obfuscation:", best.constant_obfuscation)
+    print("Obfuscation chain length:", best.obfuscation_chain_length)
+    print("Garbage blocks:", best.garbage_blocks)
+    print("Garbage length:", best.garbage_length)
     print()
-    print("Original mean heat:", original_value)
-    print("Best mean heat:", best[0])
-    print("Overhead introduced:", best[1])
+    print("Original mean heat:", original_configuration.mean_heat)
+    print(f"Best mean heat: {best.mean_heat} ({best.mean_heat / original_configuration.mean_heat * 100:.3f} %)")
+    print(f"Overhead introduced: {best.lines_num - original_configuration.lines_num} ({(best.lines_num - original_configuration.lines_num) / original_configuration.lines_num * 100:.3f} %)")
+    print("-" * 30)
 
-    execute(
-        input_file, "", 50, 
-        best[2][0], best[2][1], best[2][2], best[2][3],
-        path.dirname(__file__) + '/metrics/output.s', False, True, "best",
-        best[2][4]
-    )
-
+    # Save best run
+    best.id = "best"
+    best.evaluate()
 
 
 if __name__ == "__main__":
